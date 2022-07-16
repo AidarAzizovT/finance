ec51dd30ecac06530449646682fe220040dca9bb
Aidar <azizow37@gmail.com>
2022-07-16 18:57:04 +0300

    I've added dict with names of rows

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import pandas
+from typing import Dict, List
 
+ROWS_OF_TABLE = {'численность': 'Численность населения, занятого в экономике МО',
+                'зарплата': 'Средняя заработная плата по МО',
+                 'норматив ндфл': 'Норматив отчисления НДФЛ в бюджете МО',
+                 'доходы (план)': 'Консолидированные доходы бюджета МО (план)',
+                 'доходы (факт)': 'Консолидированные доходы бюджета МО (план)',
+                 }
 
-def make_calculation(population, salary, Nndfl, Bp, Bf):
-    ndfl = population * (salary * 0.13 * 12) / 1000
-    Db = Bp - Bf
-    PNndfl = (Nndfl / (ndfl * Nndfl / 100)) * (ndfl * Nndfl / 100 + Db)
+def make_calculation(population: float, salary: float, Nndfl: float, Bp: float, Bf: float):
+    ndfl: float = population * (salary * 0.13 * 12) / 1000
+    Db: float = Bp - Bf
+    PNndfl: float = (Nndfl / (ndfl * Nndfl / 100)) * (ndfl * Nndfl / 100 + Db)
     return PNndfl
 
 
 def read_excel(file_name):
-    data_frame_object = pandas.read_excel(file_name)
+    df = pandas.read_excel(file_name)
     try:
-        population = data_frame_object['Численность населения, занятого в экономике МО'][0]
-        salary = data_frame_object['Средняя заработная плата по МО'][0]
-        Nndfl = data_frame_object['Норматив отчисления НДФЛ в бюджете МО'][0]
-        Bp = data_frame_object['Консолидированные доходы бюджета МО (план)'][0]
-        Bf = data_frame_object['Консолидированные доходы бюджета МО (факт)'][0]
+        population = df[ROWS_OF_TABLE['численность']][0]
+        salary = df[ROWS_OF_TABLE['зарплата']][0]
+        Nndfl = df[ROWS_OF_TABLE['норматив ндфл']][0]
+        Bp = df[ROWS_OF_TABLE['доходы (план)']][0]
+        Bf = df[ROWS_OF_TABLE['доходы (факт)']][0]
         return {'population': population, 'salary': salary, 'Nndfl': Nndfl, 'Bp': Bp, 'Bf': Bf}
-    except Exception:
-        print("В файле нет необходимых данных")
+    except:
+        print("In the file is not necessary data")
+        return None
 
 
 def make_output_file(PNndfl, file_name):
@@ -26,5 +34,10 @@ def make_output_file(PNndfl, file_name):
 
 
 data_from_excel = read_excel('input data.xlsx')
-result_of_calculations = make_calculation(*data_from_excel.values())
-make_output_file(result_of_calculations, 'result.xlsx')
+
+if data_from_excel is not None:
+    result_of_calculations = make_calculation(*data_from_excel.values())
+    make_output_file(result_of_calculations, 'result.xlsx')
+    print("Successful!")
+else:
+    print('Here are errors with input data.')
