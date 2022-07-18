031d3abdd9f6f2ddf8161ecf3ae13da44b83bc07
Aidar <azizow37@gmail.com>
2022-07-18 13:46:30 +0300

    I've added module with row's names

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import pandas
 from typing import Dict
+import rows
+   
 
-ROWS_OF_TABLE = {'численность': 'Численность населения, занятого в экономике МО',
-                'зарплата': 'Средняя заработная плата по МО',
-                 'норматив ндфл': 'Норматив отчисления НДФЛ в бюджете МО',
-                 'доходы (план)': 'Консолидированные доходы бюджета МО (план)',
-                 'доходы (факт)': 'Консолидированные доходы бюджета МО (план)',
-                 }
 
 def make_calculation(population: float, salary: float, Nndfl: float, Bp: float, Bf: float) -> float:
     '''Returns result of calculation by formula
@@ -31,11 +27,11 @@ def read_excel(file_name) -> Dict:
     empty dict'''
     df = pandas.read_excel(file_name)
     try:
-        population: float = df[ROWS_OF_TABLE['численность']][0]
-        salary: float = df[ROWS_OF_TABLE['зарплата']][0]
-        Nndfl: float = df[ROWS_OF_TABLE['норматив ндфл']][0]
-        Bp: float = df[ROWS_OF_TABLE['доходы (план)']][0]
-        Bf: float = df[ROWS_OF_TABLE['доходы (факт)']][0]
+        population: float = df[rows.POPULATION][0]
+        salary: float = df[rows.SALARY][0]
+        Nndfl: float = df[rows.NORMATIV_NDFL][0]
+        Bp: float = df[rows.INCOME_PLAN][0]
+        Bf: float = df[rows.INCOME_FACT][0]
         return {'population': population, 'salary': salary, 'Nndfl': Nndfl, 'Bp': Bp, 'Bf': Bf}
     except:
         print("In the file is not necessary data")
@@ -47,14 +43,12 @@ def make_output_file(PNndfl, file_name) -> None:
     in an excel file.'''
     pandas.DataFrame({'PNndfl': [PNndfl, ]}).to_excel(file_name)
 
-#
-# data_from_excel: Dict = read_excel('input data.xlsx')
-#
-# if len(data_from_excel) != 0:
-#     result_of_calculations: float = make_calculation(*data_from_excel.values())
-#     make_output_file(result_of_calculations, 'result.xlsx')
-#     print("Successful!")
-# else:
-#     print('Here are errors with input data.')
 
-print()
+data_from_excel: Dict = read_excel('input data.xlsx')
+
+if len(data_from_excel) != 0:
+    result_of_calculations: float = make_calculation(*data_from_excel.values())
+    make_output_file(result_of_calculations, 'result.xlsx')
+    print("Successful!")
+else:
+    print('Here are errors with input data.')
